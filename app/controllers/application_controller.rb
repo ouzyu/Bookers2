1e9bbcfe94f9f8b96a1493d335c4b6e5e9419b89
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-24 08:29:39 +0000

    [Add]モデル,コントローラー,ビューの作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :authentcate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+    def after_sing_in_path_for(resource)
+      about_path
+    end
+
+    def after_sign_out_path_for(resource)
+      about_path
+    end
 
   protected
 
---
+++ app/models/book.rb
@@ -0,0 +1,5 @@
+class Book < ApplicationRecord
+
+  # belongs_to ユーザーモデルとの紐づけを行う記述を追記
+
+end
