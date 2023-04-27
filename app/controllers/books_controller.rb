62406293c56f45f547fb02ff8365cdb86e549f94
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-27 10:09:04 +0000

    [Fix]プロフィール編集のリンクの修正

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,7 +9,8 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to books_path
+    flash[:notice] = "You have created book successfully."
+    redirect_to book_path(@book.id)
   end
 
   def index
@@ -31,6 +32,7 @@ class BooksController < ApplicationController
   def update
     @book = Book.find(params[:id])
     @book.update(book_params)
+    flash[:notice] = "You have updated book successfully."
     redirect_to book_path(@book.id)
   end
 
