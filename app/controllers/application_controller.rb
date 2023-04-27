6455357bdfaddff3b6907914968c0f560eebbf80
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-27 12:07:09 +0000

    [Update]ログイン,ログアウト後の遷移先の変更,[Add]フラッシュメッセージの実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,12 @@ class ApplicationController < ActionController::Base
   before_action :authenticate_user!, except: [:top]
   before_action :configure_permitted_parameters, if: :devise_controller?
 
-    def after_sing_in_path_for(resource)
-      about_path
+    def after_sign_in_path_for(resource)
+      user_path(current_user.id)
     end
 
     def after_sign_out_path_for(resource)
-      about_path
+      root_path
     end
 
   protected
