c8cce90b6a1b834831e6c912b6ce692254b1137b
ouzyu <129580397+ouzyu@users.noreply.github.com>
2023-04-30 04:46:29 +0000

    [Fix]Rspecでのエラーの修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
-  get 'homes/about' => 'homes#about', as: 'about'
+  get 'home/about', to: 'homes#about', as: 'about'
   resources :books, except: [:new]
   resources :users, only: [:index, :show, :edit, :update]
 
